bd69b8a6307ea207b45fac53552fa9050ea1e6f9
NY <oleg.kostyukof@yandex.ru>
2022-11-28 17:19:47 +0300

    create new project

---
+++ Example_002_enter your name/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("введите ваше имя");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
+
