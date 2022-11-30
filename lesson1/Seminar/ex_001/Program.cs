eff547ddfde1ed4de29679e5c6def2812726ec8f
NY <oleg.kostyukof@yandex.ru>
2022-11-30 06:19:21 +0300

    решение 1 задачи

---
+++ lesson1/Seminar/ex_001/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("start"); //начало
+int number = 11; // определили целое число
+int result = number * number; // вычисление квадрата числа
+Console.WriteLine(result); // строка вывода результата вычислений
+Console.WriteLine("end"); //окончание 
