c83cbafd774a27d7ab3e638824b37b4c570d2e3b
NY <oleg.kostyukof@yandex.ru>
2022-11-30 05:41:34 +0300

    reconfig folder

--- lesson1/Example_002_enter your name/Program.cs
+++ lesson1/Example_002_enter your name/Program.cs
@@ -3,5 +3,3 @@ Console.WriteLine("введите ваше имя");
 string username = Console.ReadLine();
 Console.WriteLine("Привет, ");
 Console.WriteLine(username);
-
-git remote set-url origin https://github.com/kl-13-07/C-learning.git
