f8c5b99d88f57b38414ab2649339d583b003bcc9
NY <oleg.kostyukof@yandex.ru>
2022-11-30 06:38:38 +0300

    done first a task

---
+++ README.md
@@ -0,0 +1,2 @@
+# Это моя первая схема
+Вывести квадрат числа --> [Блок-схема](lesson1\Seminar\diagram.drawio.png) [Code](lesson1\Seminar\ex_001\Program.cs) 
--- lesson1/Example_002_enter your name/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("введите ваше имя");
-string username = Console.ReadLine();
-Console.WriteLine("Привет, ");
-Console.WriteLine(username);
